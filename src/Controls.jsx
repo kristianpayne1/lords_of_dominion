8288212e0b541a6b2a15e315aa53fd8142a11070
Kristian Payne <kristianpayne1@gmail.com>
2024-07-11 21:37:47 +0100

    Clamp drag controls

--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,5 +1,7 @@
 import { createContext, useRef } from "react";
 import { KeyboardControls, MapControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
 
 export const controlMap = [
     { name: "q", keys: ["q", "Q"] },
@@ -9,13 +11,29 @@ export const controlMap = [
 
 export const ControlsContext = createContext(null);
 
+const minPan = new Vector3(-5, -5, -5);
+const maxPan = new Vector3(5, 5, 5);
+
 export default function Controls({ children }) {
     const controlsRef = useRef();
+
+    useFrame(() => {
+        if (!controlsRef.current) return;
+        controlsRef.current.target.clamp(minPan, maxPan);
+    });
 
     return (
         <ControlsContext.Provider value={controlsRef}>
             <KeyboardControls map={controlMap}>
-                <MapControls enableRotate={false} ref={controlsRef} />
+                <MapControls
+                    ref={controlsRef}
+                    maxZoom={200}
+                    minZoom={50}
+                    maxAzimuthAngle={Math.PI / 4}
+                    minAzimuthAngle={Math.PI / 4}
+                    maxPolarAngle={Math.PI / 3.29}
+                    minPolarAngle={Math.PI / 3.29}
+                />
                 {children}
             </KeyboardControls>
         </ControlsContext.Provider>
